4e19e4556c61f46f15f2b658e72de6295ae1d9a7
Luigi Bungaro <luigibungaro@Luigis-iMac.local>
2019-08-17 08:41:01 +0200

    added esp. probe

--- ogame_emergency_plan.py
+++ ogame_emergency_plan.py
@@ -16,7 +16,7 @@ def ship_slow_everything_to_Saturn():
                          resources)    
 
 def ship_slow_everything_to_Mediaworld():
-    ships = [(Ships['LargeCargo'], 9999),(Ships['LightFighter'], 9999),(Ships['Cruiser'], 9999), (Ships['Recycler'], 9999)]
+    ships = [(Ships['LargeCargo'], 9999),(Ships['LightFighter'], 9999), (Ships['EspionageProbe'], 999),(Ships['Cruiser'], 9999), (Ships['Recycler'], 9999)]
     speed = Speed['10%']
     where = {'galaxy': 1, 'system': 434, 'position': 10}
     mission = Missions['Transport']
