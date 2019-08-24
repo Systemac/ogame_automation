2bf0a47bb5cd75cc9f4f338033f7848e3997420e
Luigi Bungaro <luigibungaro@Luigis-iMac.local>
2019-08-25 00:02:43 +0200

    update with new moon

--- ogame_emergency_plan.py
+++ ogame_emergency_plan.py
@@ -5,38 +5,47 @@ def log_in():
     return OGame('Indus', 'hotgigi82@hotmail.com', '1101982.Ogame')
 
 
-def ship_slow_everything_to_Moon():
-    ships = [(Ships['LargeCargo'], 999),(Ships['LightFighter'], 999),(Ships['Cruiser'], 9999), (Ships['Recycler'], 999)]
-    speed = Speed['10%']
-    where = {'galaxy': 5, 'system': 432, 'position': 8,'type':3}
+def ship_slow_everything_to_Moons(n=9999):
+    ships = [(Ships['LargeCargo'], n),(Ships['LightFighter'], n),(Ships['Cruiser'], n), (Ships['Recycler'], n)]
+    speed = Speed['60%']
+    where_Mediaworld = {'galaxy': 1, 'system': 434, 'position': 10, 'type':3}
+    where_Saturn = {'galaxy': 5, 'system': 432, 'position': 8, 'type':3}
     mission = Missions['Transport']
-    for planet_name in ['MediaWorld','Saturn','Amazon','Trony','Alibaba','Unieuro','Decathlon']:
+    for planet_name in ['Saturn','Amazon','Trony','Alibaba','Unieuro']:
         resources = {'metal': 999999999, 'crystal': 999999999, 'deuterium': 9999999999}
         ogame.send_fleet(ogame.get_planet_by_name(planet_name),
                          ships, 
                          speed, 
-                         where, 
+                         where_Mediaworld,
                          mission, 
-                         resources)    
+                         resources)
+    for planet_name in ['MediaWorld','Decathlon']:
+        resources = {'metal': 999999999, 'crystal': 999999999, 'deuterium': 9999999999}
+        ogame.send_fleet(ogame.get_planet_by_name(planet_name),
+                         ships, 
+                         speed, 
+                         where_Saturn,
+                         mission, 
+                         resources)       
 
-def ship_slow_everything_to_Mediaworld():
-    ships = [(Ships['LargeCargo'], 9999),(Ships['LightFighter'], 9999), (Ships['EspionageProbe'], 999),(Ships['Cruiser'], 9999), (Ships['Recycler'], 9999)]
-    speed = Speed['10%']
-    where = {'galaxy': 1, 'system': 434, 'position': 10, 'type':1}
-    mission = Missions['Transport']
+
+def ship_to_moon_from_moons(n=9999):
+    ships = [(Ships['LargeCargo'], n) ,(Ships['LightFighter'], n), (Ships['EspionageProbe'], n),(Ships['Cruiser'], n), (Ships['Recycler'], n)]
+    speed = Speed['50%']
+    where_Mediaworld = {'galaxy': 1, 'system': 434, 'position': 10, 'type':3}
+    where_Saturn = {'galaxy': 5, 'system': 432, 'position': 8, 'type':3}
+    mission = Missions['Park']
     resources = {'metal': 999999999, 'crystal': 999999999, 'deuterium': 9999999999}
-    ogame.send_fleet(ogame.get_moon_ids()[0],    
-                     ships, 
-                     speed, 
-                     where, 
-                     mission, 
-                     resources)
+    ogame.send_fleet(ogame.get_moon_ids()[1], ships, speed, where_Mediaworld, mission, resources) 
+    ogame.send_fleet(ogame.get_moon_ids()[0], ships, speed, where_Saturn, mission, resources) 
+ 
+
 
 if __name__=="__main__":    
     ogame = log_in()
-    if ogame.is_under_attack():
-        ship_slow_everything_to_Mediaworld()
-        ship_slow_everything_to_Moon()
+    if not ogame.is_under_attack():
+        ship_to_moon_from_moons()
+        ship_slow_everything_to_Moons()
         ogame.logout()
     else:
         ogame.logout()
